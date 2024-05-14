0ae610638566c580fa7142e4d170d0303af3252c
Mai2aa <mysaadil0@gmail.com>
2024-05-14 01:26:00 -0700

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-""" a class with 3 methids inside it """
+    """ a class with 3 methids inside it """
 
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class Square:
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -28,4 +28,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
